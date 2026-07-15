f7527d7233a31350fd766b457ad5ee2a2b5d97cf
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-07-15 16:39:32 -0400

    Initial Commit

---
+++ GhibliSwiftUIApp/GhibliSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUIAppApp.swift
+//  GhibliSwiftUIApp
+//
+//  Created by Lidiia Diachkovskaia on 7/15/26.
+//
+
+import SwiftUI
+
+@main
+struct GhibliSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
