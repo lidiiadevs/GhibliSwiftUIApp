19565ca15c76788ee165281936593f80549d3197
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-08-04 18:57:31 -0400

    add favorites

---
+++ GhibliSwiftUIApp/Favorites/Services/FavoriteStorage.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteStorage.swift
+//  GhibliSwiftUIApp
+//
+//  Created by Lidiia Diachkovskaia on 8/3/26.
+//
+
+import Foundation
---
+++ GhibliSwiftUIApp/Favorites/Services/MockFavoriteStorage.swift
@@ -0,0 +1,8 @@
+//
+//  MockFavoriteStorage.swift
+//  GhibliSwiftUIApp
+//
+//  Created by Lidiia Diachkovskaia on 8/3/26.
+//
+
+import Foundation
